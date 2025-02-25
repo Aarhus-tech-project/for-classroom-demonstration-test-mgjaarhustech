3630d2d76a2108c68fd0256039edf65985d5e04f
Morten Godrim Jensen <mgj@aarhustech.dk>
2025-02-25 13:18:25 +0100

    Comments in code

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+// I did a merge into the feedback and it clossed the pull request now I need a
+// new commit to make a new pull request.
