c1cdd1e71dc75e853a8e69b871cea16a795db0f9
Morten Godrim Jensen <mgj@aarhustech.dk>
2025-02-25 13:21:51 +0100

    Second comment

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -3,3 +3,5 @@ Console.WriteLine("Hello, World!");
 
 // I did a merge into the feedback and it clossed the pull request now I need a
 // new commit to make a new pull request.
+
+// I made the pull request and added a comment I will now do another commit and we if i can make a new comment
